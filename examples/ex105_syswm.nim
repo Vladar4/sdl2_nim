fd0e20942f893ac68a712aba9b4c2826d18d8ddc
Vladar4 <vladar4@gmail.com>
2016-04-12 09:23:49 +0300

    fix: ex105_syswm.nim

--- examples/ex105_syswm.nim
+++ examples/ex105_syswm.nim
@@ -6,6 +6,22 @@
 
 import sdl2/sdl, sdl2/sdl_syswm
 
+
+proc subsys(kind: SysWMKind): string =
+  case kind
+  of SysWM_Unknown: "an unknown system"
+  of SysWM_Windows: "Microsoft Windows"
+  of SysWM_X11: "X Window System"
+  of SysWM_DirectFB: "DirectFB"
+  of SysWM_Cocoa: "Apple OS X"
+  of SysWM_UIKit: "UIKit"
+  of SysWM_Wayland: "Wayland"
+  of SysWM_Mir: "Mir"
+  of SysWM_WinRT: "Microsoft Windows RT"
+  of SysWM_Android: "Android"
+
+
+
 discard sdl.init(0)
 var window = sdl.createWindow("", 0, 0, 0, 0, sdl.WindowHidden)
 var info: SysWMinfo
@@ -13,24 +29,11 @@ var info: SysWMinfo
 version(info.version)
 
 if getWindowWMInfo(window, addr(info)):
-  var subsys: string
-  case info.subsystem:
-  of SysWM_Unknown: subsys = "an unknown system"
-  of SysWM_Windows: subsys = "Microsoft Windows"
-  of SysWM_X11: subsys = "X Window System"
-  of SysWM_DirectFB: subsys = "DirectFB"
-  of SysWM_Cocoa: subsys = "Apple OS X"
-  of SysWM_UIKit: subsys = "UIKit"
-  of SysWM_Wayland: subsys = "Wayland"
-  of SysWM_Mir: subsys = "Mir"
-  of SysWM_WinRT: subsys = "Microsoft Windows RT"
-  of SysWM_Android: subsys = "Android"
-
   echo "This program is running SDL version " &
     $info.version.major.int & "." &
     $info.version.minor.int & "." &
     $info.version.patch.int &
-    " on " & $subsys & "."
+    " on " & subsys(info.subsystem) & "."
 
 else:
   echo "Couldn't get window information : " & $sdl.geterror()
