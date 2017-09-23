0a9bf11abe444996248f6e59f82c0daffd53d271
Vladar4 <vladar4@gmail.com>
2017-09-23 12:44:04 +0300

    updated blendmode.nim

--- sdl2/private/blendmode.nim
+++ sdl2/private/blendmode.nim
@@ -27,6 +27,9 @@
 type
   BlendMode* {.size: sizeof(cint).} = enum ##  \
     ##  The blend mode used in renderCopy() and drawing operations.
+    ##
+    ##  Additional custom blend modes
+    ##  can be returned by ``composeCustomBlendMode()``.
     BLENDMODE_NONE = 0x00000000, ##  \
       ##  no blending
       ##
@@ -43,9 +46,57 @@ type
       ##  dstRGB = (srcRGB * srcA) + dstRGB
       ##
       ##  dstA = dstA
-    BLENDMODE_MOD = 0x00000004  ##  \
+    BLENDMODE_MOD = 0x00000004, ##  \
       ##  color modulate
       ##
       ##  dstRGB = srcRGB * dstRGB
       ##
       ##  dstA = dstA
+    BLENDMODE_INVALID = 0x7FFFFFFF
+
+  BlendOperation* {.size: sizeof(cint).} = enum ##  \
+    ##  The blend operation used when combining
+    ##  source and destination pixel components.
+    BLENDOPERATION_ADD          = 0x1,  ##  \
+      ##  dst + src : supported by all renderers
+    BLENDOPERATION_SUBTRACT     = 0x2,  ##  \
+      ##  dst - src : supported by D3D9, D3D11, OpenGL, OpenGLES
+    BLENDOPERATION_REV_SUBTRACT = 0x3,  ##  \
+      ##  src - dst : supported by D3D9, D3D11, OpenGL, OpenGLES
+    BLENDOPERATION_MINIMUM      = 0x4,  ##  \
+      ##  min(dst, src) : supported by D3D11
+    BLENDOPERATION_MAXIMUM      = 0x5   ##  \
+      ##  max(dst, src) : supported by D3D11
+
+  BlendFactor* {.size: sizeof(cint).} = enum  ##  \
+    ##  The normalized factor used to multiply pixel components.
+    BLENDFACTOR_ZERO                = 0x1,  ##  0, 0, 0, 0
+    BLENDFACTOR_ONE                 = 0x2,  ##  1, 1, 1, 1
+    BLENDFACTOR_SRC_COLOR           = 0x3,  ##  srcR, srcG, srcB, srcA
+    BLENDFACTOR_ONE_MINUS_SRC_COLOR = 0x4,  ##  1-srcR, 1-srcG, 1-srcB, 1-srcA
+    BLENDFACTOR_SRC_ALPHA           = 0x5,  ##  srcA, srcA, srcA, srcA
+    BLENDFACTOR_ONE_MINUS_SRC_ALPHA = 0x6,  ##  1-srcA, 1-srcA, 1-srcA, 1-srcA
+    BLENDFACTOR_DST_COLOR           = 0x7,  ##  dstR, dstG, dstB, dstA
+    BLENDFACTOR_ONE_MINUS_DST_COLOR = 0x8,  ##  1-dstR, 1-dstG, 1-dstB, 1-dstA
+    BLENDFACTOR_DST_ALPHA           = 0x9,  ##  dstA, dstA, dstA, dstA
+    BLENDFACTOR_ONE_MINUS_DST_ALPHA = 0xA   ##  1-dstA, 1-dstA, 1-dstA, 1-dstA
+
+proc composeCustomBlendMode*(
+    srcColorFactor: BlendFactor,
+    dstColorFactor: BlendFactor,
+    colorOperation: BlendOperation,
+    srcAlphaFactor: BlendFactor,
+    dstAlphaFactor: BlendFactor,
+    alphaOperation: BlendOperation): BlendMode {.
+    cdecl, importc: "SDL_ComposeCustomBlendMode", dynlib: SDL2_LIB.}
+  ##  Create a custom blend mode,
+  ##  which may or may not be supported by a given renderer.
+  ##
+  ##  The result of the blend mode operation will be:
+  ##
+  ##  dstRGB = dstRGB * dstColorFactor colorOperation srcRGB * srcColorFactor
+  ##
+  ##  and
+  ##
+  ##  dstA = dstA * dstAlphaFactor alphaOperation srcA * srcAlphaFactor
+
