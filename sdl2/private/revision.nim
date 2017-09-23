4d34aa0e2c26faa310dc1c7be1936c9a0c5b330c
Vladar4 <vladar4@gmail.com>
2017-09-23 11:49:37 +0300

    updated copyrights and version info

--- sdl2/private/revision.nim
+++ sdl2/private/revision.nim
@@ -4,5 +4,5 @@
 ##  This header defines the current SDL revision.
 
 const
-  REVISION* = "hg-10556:007dfe83abf8"
-  REVISION_NUMBER* = 10556
+  REVISION* = "hg-11524:8df7a59b5528"
+  REVISION_NUMBER* = 11524
